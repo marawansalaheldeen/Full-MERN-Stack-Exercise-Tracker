1a2f60deaf50f7fc8f1a880b55f0d2f728f46d56
MS <marawansalaheldeen@gmail.com>
2020-06-17 23:54:01 +0200

    creating front-end components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router , Route } from 'react-router-dom';
+
+import Navbar from './components/navbar';
+import ExercisesList from './components/exerciselist';
+import CreateUser from './components/createuser';
+import CreateExercise from './components/createexercise';
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <Router>
+      <div className="container">
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
